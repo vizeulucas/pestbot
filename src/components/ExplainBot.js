7953f2d3137dbe7f48e3d2985490b8f3a3db4ab4
Lucas Casimiro de Souza Vizeu <42391161+vizeulucas@users.noreply.github.com>
2020-08-23 15:22:19 -0300

    Update ExplainBot.js

--- src/components/ExplainBot.js
+++ src/components/ExplainBot.js
@@ -6,7 +6,7 @@ const ExplainBot = () => {
         <div id="explain-bot">
             <div className="explain-text">
                 <h1>Como o Pest<span>Bot</span> Funciona</h1>
-                <p>O PestBot é uma inteligência articial que utiliza recursos do IBM Watson Studio. Com isso, utilizamos a inteligência artificial da melhor forma possível e com conhecimento em pragas. Dessa maneira, ela se adapta, aprende com as perguntas e gera respostas  em tempo real, entregando informações úteis para o agricultor sobre pragas e métodos de controle. Basicamente, ensinamos ele a pensar  utilizando dados extraídos de livros e pesquisas, mantendo uma base atualizada e com novos aprendizados a cada nova interação.</p>    
+                <p>O <span>PestBot é uma inteligência articial</span> que utiliza recursos do IBM Watson Studio. Com isso, utilizamos a inteligência artificial da melhor forma possível e com conhecimento em <span>pragas</span>. Dessa maneira, ela se adapta, aprende com as perguntas e gera respostas  em tempo real, entregando informações úteis para o agricultor sobre pragas e métodos de controle. Basicamente, ensinamos ele a pensar  utilizando dados extraídos de livros e pesquisas, mantendo uma <span>base atualizada</span> e com novos aprendizados a cada nova interação.</p>    
             </div>
             <ExplainSvg />
         </div>
